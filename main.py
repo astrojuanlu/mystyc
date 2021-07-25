e1bf3da3a50643ec9c36c29e2d5db081bf2a5428
Juan Luis Cano Rodríguez <hello@juanlu.space>
2021-07-25 20:26:25 +0200

    Basic templating

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from rst_to_myst import rst_to_myst
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 class InputReST(BaseModel):
@@ -23,3 +26,8 @@ async def convert(input_rest: InputReST):
         use_sphinx=False,
     )
     return {"input_rest": input_rest, "output_myst": output.text}
+
+
+@app.get("/convert", response_class=HTMLResponse)
+async def convert(request: Request):
+    return templates.TemplateResponse("convert.html", {"request": request})
